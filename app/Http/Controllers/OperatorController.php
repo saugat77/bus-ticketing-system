339da52999525f56957a620b2bcfabee7153d489
= <saugatpandey4@gmail.com>
2022-09-26 16:30:27 +0545

    adding css and admin panel

---
+++ app/Http/Controllers/OperatorController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Operators;
+use Illuminate\Http\Request;
+
+class OperatorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $operators = Operators::all();
+       return view('admin.operators.operator-list',compact('operators'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'operator_name' => 'required',
+             'operator_email' => 'required',
+             'operator_address' => 'required',
+             'operator_phone' => 'required',
+             'operator_logo' => 'image|max:2048',
+        ]);
+
+            $image =  $request->file('operator_logo');
+
+            $image_name = rand() . '.' . $image->getClientOriginalExtension();
+            $image->move(public_path('operator_images'), $image_name);
+
+            $operators = new Operators;
+            $operators->operator_name = $request->operator_name;
+            $operators->operator_email = $request->operator_email;
+            $operators->operator_address = $request->operator_address;
+            $operators->operator_phone = $request->operator_phone;
+            $operators->operator_logo = $image_name;
+
+                // dd($operators);
+            $operators->save(); // THIS SAVE FUNCTION WILL SAVE THE DATA OKAY
+
+            return redirect()->back()->with('flash_message_success', 'Operator Added Succssfully!');
+// WE NEED TO GENERATE THIS CUSTOM FLASH MESSAGE OKAY. SO LET'S ADD THAT FIRST BEFORE INSERTING OKAY.
+            $id = $request::get('operator_id');
+            $operators = Operator::where('operator_id', $id);
+
+            return view('admin.operators.operator-list', compact('operators'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
